9869eeac2304a71bd63dd7dae2d694114f435c6d
Nikolay Elshin <nikolay19830223@gmail.com>
2023-12-25 15:05:53 +0800

    Test to work in git

--- app/src/main/java/ru/nelshin/telegram/MainActivity.kt
+++ app/src/main/java/ru/nelshin/telegram/MainActivity.kt
@@ -1,39 +1,26 @@
 package ru.nelshin.telegram
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import com.theartofdev.edmodo.cropper.CropImage
 import ru.nelshin.telegram.activities.RegisterActivity
 import ru.nelshin.telegram.databinding.ActivityMainBinding
-import ru.nelshin.telegram.models.User
 import ru.nelshin.telegram.ui.fragments.ChatsFragment
 import ru.nelshin.telegram.ui.objects.AppDrawer
 import ru.nelshin.telegram.utilits.APP_ACTIVITY
 import ru.nelshin.telegram.utilits.AUTH
 import ru.nelshin.telegram.utilits.AppStates
-import ru.nelshin.telegram.utilits.AppValueEventListener
-import ru.nelshin.telegram.utilits.CHILD_PHOTO_URL
-import ru.nelshin.telegram.utilits.NODE_USERS
-import ru.nelshin.telegram.utilits.REF_DATABASE_ROOT
-import ru.nelshin.telegram.utilits.CURRENT_UID
-import ru.nelshin.telegram.utilits.FOLDER_PROFILE_IMAGE
-import ru.nelshin.telegram.utilits.REF_STORAGE_ROOT
-import ru.nelshin.telegram.utilits.USER
 import ru.nelshin.telegram.utilits.initFarebase
 import ru.nelshin.telegram.utilits.initUser
 import ru.nelshin.telegram.utilits.replaceActivity
 import ru.nelshin.telegram.utilits.replaceFragment
-import ru.nelshin.telegram.utilits.showToast
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
     lateinit var mAppDrawer: AppDrawer
     private lateinit var mToolbar: Toolbar
+    //private lateinit var test:String
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
