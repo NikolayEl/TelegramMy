18370a71823684333eac763d362012856f1e8e4b
Nikolay Elshin <nikolay19830223@gmail.com>
2024-01-25 21:51:09 +0800

    Recording a group in database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "ru.nelshin.telegram"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -35,34 +35,34 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures{
+    buildFeatures {
         viewBinding = true
     }
 }
 
 dependencies {
 
-        implementation ("com.google.android.material:material:x.x.x")
-    implementation ("com.firebaseui:firebase-ui-database:8.0.2")
-    implementation ("com.squareup.picasso:picasso:2.8")
-    api ("com.theartofdev.edmodo:android-image-cropper:2.8.0")
-    implementation ("com.google.firebase:firebase-appindexing:20.0.0")
+    implementation("com.google.android.material:material:x.x.x")
+    implementation("com.firebaseui:firebase-ui-database:8.0.2")
+    implementation("com.squareup.picasso:picasso:2.8")
+    api("com.theartofdev.edmodo:android-image-cropper:2.8.0")
+    implementation("com.google.firebase:firebase-appindexing:20.0.0")
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
-    implementation ("androidx.browser:browser:1.7.0")
-    implementation ("de.hdodenhof:circleimageview:3.1.0")
-    implementation ("com.mikepenz:materialdrawer-nav:7.0.0")
-    implementation ("com.mikepenz:materialdrawer:7.0.0")
-    implementation ("androidx.appcompat:appcompat:1.6.1")
-    implementation ("androidx.recyclerview:recyclerview:1.3.2")
-    implementation ("androidx.annotation:annotation:1.7.1")
-    implementation ("com.google.android.material:material:1.11.0")
-    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.browser:browser:1.7.0")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation("com.mikepenz:materialdrawer-nav:7.0.0")
+    implementation("com.mikepenz:materialdrawer:7.0.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.annotation:annotation:1.7.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-auth:22.3.0")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation("com.google.firebase:firebase-storage:20.3.0")
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/ru/nelshin/telegram/ui/screens/groups/CreateGroupFragment.kt
+++ app/src/main/java/ru/nelshin/telegram/ui/screens/groups/CreateGroupFragment.kt
@@ -1,31 +1,57 @@
 package ru.nelshin.telegram.ui.screens.groups
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.widget.EditText
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
+import de.hdodenhof.circleimageview.CircleImageView
 import ru.nelshin.telegram.R
+import ru.nelshin.telegram.database.createGroupToDatabase
 import ru.nelshin.telegram.models.CommonModel
 import ru.nelshin.telegram.ui.screens.base.BaseFragment
+import ru.nelshin.telegram.ui.screens.main_list.MainListFragment
 import ru.nelshin.telegram.utilits.APP_ACTIVITY
 import ru.nelshin.telegram.utilits.getPlurals
 import ru.nelshin.telegram.utilits.hideKeyboard
+import ru.nelshin.telegram.utilits.replaceFragment
 import ru.nelshin.telegram.utilits.showToast
 
 class CreateGroupFragment(private var listContacts:List<CommonModel>):BaseFragment(R.layout.fragment_create_group) {
 
     private lateinit var mRecyclerView: RecyclerView
     private lateinit var mAdapter: AddContactsAdapter
+    private var mUri = Uri.EMPTY
     override fun onResume() {
         super.onResume()
         APP_ACTIVITY.title = getString(R.string.create_group)
         hideKeyboard()
         initRecyclerView()
+        APP_ACTIVITY.findViewById<CircleImageView>(R.id.create_group_photo).setOnClickListener { addPhoto() }
         APP_ACTIVITY.findViewById<FloatingActionButton>(R.id.create_group_btn_complete).setOnClickListener {
-            showToast("click")
+            val nameGroup = APP_ACTIVITY.findViewById<EditText>(R.id.create_group_input_name).text.toString()
+            if(nameGroup.isEmpty()){
+                showToast(getString(R.string.enter_group_name))
+            } else {
+                createGroupToDatabase(nameGroup, mUri, listContacts){
+                    replaceFragment(MainListFragment())
+                }
+            }
         }
         APP_ACTIVITY.findViewById<EditText>(R.id.create_group_input_name).requestFocus()
         APP_ACTIVITY.findViewById<TextView>(R.id.create_group_counts).text = getPlurals(listContacts.size)
+    }
+
+    private fun addPhoto() {
+        CropImage.activity()
+            .setAspectRatio(1, 1)
+            .setRequestedSize(250, 250)
+            .setCropShape(CropImageView.CropShape.OVAL)
+            .start(APP_ACTIVITY, this)
     }
 
     private fun initRecyclerView() {
@@ -33,5 +59,14 @@ class CreateGroupFragment(private var listContacts:List<CommonModel>):BaseFragme
         mAdapter = AddContactsAdapter()
         mRecyclerView.adapter = mAdapter
         listContacts.forEach { mAdapter.updateListItems(it) }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null){
+            mUri = CropImage.getActivityResult(data).uri
+            APP_ACTIVITY.findViewById<CircleImageView>(R.id.create_group_photo).setImageURI(mUri)
+        }
     }
 }
